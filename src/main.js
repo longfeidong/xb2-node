27d892567337c8c64326106f725595fab41ed885
donglongfei <donglongfei2011@gmail.com>
2020-08-01 15:56:15 +0800

    使用express框架开发应用接口

--- src/main.js
+++ src/main.js
@@ -1,82 +1,44 @@
-// const greeting = '你好，小白兔';
-// console.log(greeting);
+// 使用express创建Web服务器
+const express = require('express');
+const { response } = require('express');
+const app = express();
+const port = 3000;
 
-// const fn = (breakfast) => {
-//     console.log('早餐：', breakfast);
-// }
+app.listen(port, () => {
+    console.log('服务已启动');
+})
 
-const fn = (fan, food) => {
-    console.log(fan, food);
-}
-fn('早饭', '豆浆油条');
-fn('午饭', '大盘鸡');
+app.get('/', (request, response) => {
+    response.send('Welcome express');
+})
 
-const greet = (name) => {
-    return '你好，' + name
-}
-
-const greeting = greet('阿远');
-
-console.log(greeting);
-
-const book = {
-    title: '三体',
-    toString() {
-        return `《${this.title}》`;
+const data = [
+    {
+        id: 1,
+        title: 'HTML',
+        content: '超文本标记语言'
+    },
+    {
+        id: 2,
+        title: 'CSS',
+        content: '层叠样式表'
+    },
+    {
+        id: 3,
+        title: 'JavaScript',
+        content: '全栈开发语言'
     }
-};
-console.log(book.toString());
+]
 
-const obj = {
-    'title': '时代楷模',
-    'name': '张富清'
-}
-// 解构对象，可以定义别名
-const { title: rongyu, name } = obj;
-console.log(rongyu, name);
+app.get('/posts', (request, response) => {
+    response.send(data);
+})
 
-const fruits = ['苹果', '香蕉', '橙子'];
-const [apple, banna, orange] = fruits;
-console.log(apple, banna, orange);
-
-const city1 = ['济宁', '菏泽', '枣庄'];
-const city2 = ['济南', '泰安', '淄博'];
-const sd = [...city1, ...city2];
-console.log(sd);
-
-const data = {
-    title: '四世同堂'
-};
-
-const author = {
-    name: '老舍'
-};
-
-const work = {
-    ...data,
-    author
-};
-
-console.log(work)
-
-class Car {
-    // brank;
-
-    constructor(brank) {
-        this.brank = brank;
-    }
-
-    driver() {
-        console.log('嘀~嘀~')
-    }
-}
-
-const c1 = new Car('Benz');
-const c2 = new Car('BMW');
-c1.driver();
-console.log(c1, c2);
-
-class Suv extends Car {}
-const s1 = new Suv('Audi');
-s1.driver();
-console.log(s1)
+app.get('/posts/:postId', (request, response) => {
+    // 获取内容ID
+    const { postId } = request.params;
+    // 查询对应的数据
+    const results = data.filter(item => item.id == postId)
+    // 作出相应
+    response.send(results[0]);
+})
