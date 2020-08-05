1d55d3ff42c001435dc36958b3c9a0acc5250014
donglongfei <donglongfei2011@gmail.com>
2020-08-05 15:21:02 +0800

    http交换数据

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const { response } = require('express');
 const app = express();
 const port = 3000;
 
+/**
+ * 使用 JSON 中间件
+ * 使用后服务端可以直接处理客户端发送过来的 JSON 数据
+ */
+app.use(express.json());
+
 app.listen(port, () => {
     console.log('服务已启动');
 })
@@ -41,4 +47,22 @@ app.get('/posts/:postId', (request, response) => {
     const results = data.filter(item => item.id == postId)
     // 作出相应
     response.send(results[0]);
+})
+
+/**
+ * 创建内容
+ */
+app.post('/posts', (request, response) => {
+    // 获取请求数据
+    const { content } = request.body;
+    // 设置响应的状态码
+    response.status('201');
+    // 输出请求的头部数据
+    console.log(request.headers.token);
+    // 设置服务端响应的头部数据
+    response.set('Token', 112233);
+    // 作出响应，express会把客户端发送过来的数据处理成json，然后再发送给客户端
+    response.send({
+        message: `成功创建了内容：${content} `
+    })
 })
