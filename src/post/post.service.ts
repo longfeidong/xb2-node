a85f625857e3491f67dfac8cd9ca4a2521d07d57
donglongfei <donglongfei2011@gmail.com>
2020-08-27 14:38:20 +0800

    创建内容资源的服务

---
+++ src/post/post.service.ts
@@ -0,0 +1,19 @@
+/**
+ * 获取内容列表
+ */
+
+export const getPosts = () => {
+  const data = [
+    {
+      content: 'm15社区',
+    },
+    {
+      content: '运河壹号',
+    },
+    {
+      content: '鲁宁重工',
+    },
+  ];
+
+  return data;
+};
