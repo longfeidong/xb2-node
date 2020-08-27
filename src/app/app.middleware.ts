8e833569ee08041a226f2079a79329d9dd022a69
donglongfei <donglongfei2011@gmail.com>
2020-08-27 15:42:05 +0800

    创建默认的异常处理器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -16,3 +16,25 @@ export const requestUrl = (
   console.log('请求地址：', request.url);
   next();
 };
+
+/**
+ * 默认异常处理器
+ */
+export const defaultErrorHandler = (
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  let statusCode: number, message: string;
+  /**
+   * 处理异常
+   */
+  switch (error.message) {
+    default:
+      statusCode = 500;
+      message = '服务器出现错误';
+      break;
+  }
+  response.status(statusCode).send({ message });
+};
