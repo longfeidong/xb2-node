52e18525a25d6fefe7758f5863d5466610e0f4db
donglongfei <donglongfei2011@gmail.com>
2020-09-03 14:14:43 +0800

    测试连接数据仓库

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import app from './app/index';
 import { APP_PORT } from './app/app.config';
+import { connection } from './app/database/mysql';
 
 app.listen(APP_PORT, () => {
   console.log('服务已启动！');
+});
+
+/**
+ * 测试连接数据服务
+ */
+connection.connect(error => {
+  if (error) {
+    console.log('数据库连接失败~~：', error.message);
+    return;
+  }
+  console.log('数据库连接成功！');
 });
