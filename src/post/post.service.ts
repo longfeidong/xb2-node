eca95b2c48e55cf0d8302c1eadc7b28592eb2cf0
donglongfei <donglongfei2011@gmail.com>
2020-09-03 14:58:09 +0800

    利用数据关系查询数据

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -15,7 +15,19 @@ export const getPosts = async () => {
   //     content: '鲁宁重工',
   //   },
   // ];
-  const statement = `SELECT * FROM post`;
+  const statement = `
+    SELECT 
+      post.id,
+      post.title,
+      post.content,
+      JSON_OBJECT(
+        'id', user.id,
+        'name', user.name
+      ) as user
+    FROM post
+    LEFT JOIN user
+      ON user.id = post.userId
+  `;
   // await必须写在async函数内部
   const [data] = await connection.promise().query(statement);
 
