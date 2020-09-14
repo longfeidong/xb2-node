28df9043992698b67e85d8e63b444e5b0fa41f07
donglongfei <donglongfei2011@gmail.com>
2020-09-14 15:47:06 +0800

    定义创建用户的接口

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,20 @@
 import { connection } from '../app/database/mysql';
 import { UserModel } from './user.model';
 // 定义user需要的服务方法
+
+/**
+ * 创建用户
+ */
+export const createUser = async (user: UserModel) => {
+  // 准备查询
+  const statement = `
+        INSERT INTO user
+        SET ?
+    `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, user);
+
+  // 提供数据
+  return data;
+};
